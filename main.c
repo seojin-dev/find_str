9b892975557d73f3a8976e16a73b7d1326beb326
Seojin Kim <148533834+seojindeveloper@users.noreply.github.com>
2024-03-24 18:38:25 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    printf("수령동지의 찬양을") ;
+    Sleep(1000);
+
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    printf("글자렬 찾기 써비스") ;
+    Sleep(1000);
+    printf(".") ;
+    Sleep(1000);
+    printf("..") ;
+    Sleep(1000);
+    printf("...") ;
+    Sleep(1000);
+    printf("....") ;
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    Sleep(1000);
+    printf("준") ;
+    Sleep(100);
+    printf("비") ;
+    Sleep(100);
+    printf("하") ;
+    Sleep(100);
+    printf("고") ;
+    Sleep(100);
+    printf("있") ;
+    Sleep(100);
+    printf("으") ;
+    Sleep(100);
+    printf("니") ;
+    Sleep(100);
+    printf(" ") ;
+    Sleep(100);
+    printf("기") ;
+    Sleep(100);
+    printf("다") ;
+    Sleep(100);
+    printf("려") ;
+    Sleep(100);
+    printf(" ") ;
+    Sleep(100);
+    printf("주") ;
+    Sleep(100);
+    printf("십") ;
+    Sleep(100);
+    printf("시") ;
+    Sleep(100);
+    printf("오") ;
+    Sleep(100);
+    printf(" ") ;
+    Sleep(100);
+    printf("동") ;
+    Sleep(100);
+    printf("무") ;
+    Sleep(1000);
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    char a[100], b ;
+    int cnt=0, i ;
+    printf("글자렬을 입력하십시요 동무 : ") ;
+    scanf("%s", a) ;
+    printf("찾을 글자를 입구하십시요 동무 :") ;
+    scanf(" %c", &b) ;
+    for(i=0;a[i]!=NULL;i++){
+        if(a[i]==b){
+            printf("%c", b) ;
+            Sleep(100);
+            printf("문") ;
+            Sleep(100);
+            printf("자") ;
+            Sleep(100);
+            printf(" ") ;
+            Sleep(100);
+            printf("%d", i+1) ;
+            Sleep(100);
+            printf("번") ;
+            Sleep(100);
+            printf(" ") ;
+            Sleep(100);
+            printf("째") ;
+            Sleep(100);
+            printf("에") ;
+            Sleep(100);
+            printf(" ") ;
+            Sleep(100);
+            printf("있") ;
+            Sleep(100);
+            printf("습") ;
+            Sleep(100);
+            printf("니") ;
+            Sleep(100);
+            printf("다") ;
+            Sleep(100);
+            printf(".") ;
+
+        }
+    }
 }
