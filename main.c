66a44581e7624c3a0ab05bcf532bc041cee1fcc7
Seojin Kim <148533834+seojin-dev@users.noreply.github.com>
2024-11-03 12:19:32 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,65 +4,12 @@
 
 int main()
 {
-    printf("수령동지의 찬양을") ;
-    Sleep(1000);
-
-    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-    printf("글자렬 찾기 써비스") ;
-    Sleep(1000);
-    printf(".") ;
-    Sleep(1000);
-    printf("..") ;
-    Sleep(1000);
-    printf("...") ;
-    Sleep(1000);
-    printf("....") ;
-    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-    Sleep(1000);
-    printf("준") ;
-    Sleep(100);
-    printf("비") ;
-    Sleep(100);
-    printf("하") ;
-    Sleep(100);
-    printf("고") ;
-    Sleep(100);
-    printf("있") ;
-    Sleep(100);
-    printf("으") ;
-    Sleep(100);
-    printf("니") ;
-    Sleep(100);
-    printf(" ") ;
-    Sleep(100);
-    printf("기") ;
-    Sleep(100);
-    printf("다") ;
-    Sleep(100);
-    printf("려") ;
-    Sleep(100);
-    printf(" ") ;
-    Sleep(100);
-    printf("주") ;
-    Sleep(100);
-    printf("십") ;
-    Sleep(100);
-    printf("시") ;
-    Sleep(100);
-    printf("오") ;
-    Sleep(100);
-    printf(" ") ;
-    Sleep(100);
-    printf("동") ;
-    Sleep(100);
-    printf("무") ;
-    Sleep(1000);
-    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    printf("문자열찾기 프로그램") ;
     char a[100], b ;
     int cnt=0, i ;
-    printf("글자렬을 입력하십시요 동무 : ") ;
+    printf("문자열을 입력하십시오. : ") ;
     scanf("%s", a) ;
-    printf("찾을 글자를 입구하십시요 동무 :") ;
+    printf("찾을 글자를 입력하십시오. :") ;
     scanf(" %c", &b) ;
     for(i=0;a[i]!=NULL;i++){
         if(a[i]==b){
